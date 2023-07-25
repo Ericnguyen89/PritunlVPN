fbf271acf2ddbfdd3123c6245db695624e2251dd
Ericnguyen89 <127569176+Ericnguyen89@users.noreply.github.com>
2023-07-25 18:06:07 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,5 +9,5 @@ EOF
 sudo apt --assume-yes install gnupg
 gpg --keyserver hkp://keyserver.ubuntu.com --recv-keys 7568D9BB55FF9E5287D586017AE645C0CF8E292A
 gpg --armor --export 7568D9BB55FF9E5287D586017AE645C0CF8E292A | sudo tee /etc/apt/trusted.gpg.d/pritunl.asc
-sudo apt update
-sudo apt install pritunl-client-electron
+sudo apt update -y
+sudo apt install pritunl-client-electron -y
