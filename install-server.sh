5cb6401eb6354f57d4863221495b4018213a35c1
Ericnguyen89 <127569176+Ericnguyen89@users.noreply.github.com>
2023-09-05 15:45:47 +0700

    Update install-server.sh

--- install-server.sh
+++ install-server.sh
@@ -1,1 +1,36 @@
+#!/bin/bash
+
+# Function to check if a command is available
+command_exists() {
+  command -v "$1" >/dev/null 2>&1
+}
+
+# Function to determine the Linux distribution
+get_linux_distribution() {
+  if command_exists lsb_release; then
+    lsb_release -si
+  elif [ -f /etc/os-release ]; then
+    source /etc/os-release
+    echo "$ID"
+  else
+    echo "Unknown"
+  fi
+}
+
+# Get the Linux distribution
+linux_distribution=$(get_linux_distribution)
+
+# Check if the Linux distribution is one of the specified ones
+if [[ $linux_distribution =~ ^(OracleLinux|AlmaLinux|Rocky|rhel|RedHat)$ ]]; then
+  # Install rocky.sh for Oracle Linux, AlmaLinux, Rocky Linux, or RHEL
+  echo "Installing rocky.sh..."
+  ./rocky.sh
+elif [[ $linux_distribution =~ ^(Ubuntu)$ ]]; then
+  # Install ubuntu.sh for Ubuntu
+  echo "Installing ubuntu.sh..."
+  ./ubuntu.sh
+else
+  echo "Unsupported Linux distribution: $linux_distribution"
+  exit 1
+fi
 
