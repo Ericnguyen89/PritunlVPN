47ca6dbc1577bcdff47ee8f553a2384e926dff93
Ericnguyen89 <127569176+Ericnguyen89@users.noreply.github.com>
2023-09-05 15:57:33 +0700

    Update install-server.sh

--- install-server.sh
+++ install-server.sh
@@ -21,7 +21,7 @@ get_linux_distribution() {
 linux_distribution=$(get_linux_distribution)
 
 # Check if the Linux distribution is one of the specified ones
-if [[ $linux_distribution =~ ^(OracleLinux|AlmaLinux|Rocky|rhel|RedHat)$ ]]; then
+if [[ $linux_distribution =~ ^(Rocky)$ ]]; then
   # Install rocky.sh for Oracle Linux, AlmaLinux, Rocky Linux, or RHEL
   echo "Installing rocky.sh..."
   wget https://raw.githubusercontent.com/Ericnguyen89/PriturnVPN/main/server-rocky.sh && sudo bash server-rocky.sh
