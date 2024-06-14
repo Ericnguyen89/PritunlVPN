b9b99f36a0404bb27243b59eeb546001e226d6a8
Ericnguyen89 <127569176+Ericnguyen89@users.noreply.github.com>
2024-06-14 09:02:47 +0700

    Update and rename client-rocky.sh to client-rocky-almaLinux.sh

--- Client/client-rocky-almaLinux.sh
+++ Client/client-rocky-almaLinux.sh
@@ -1,19 +1,17 @@
-sudo tee /etc/yum.repos.d/pritunl.repo << EOF
+sudo tee -a /etc/yum.repos.d/pritunl.repo << EOF
 [pritunl]
 name=Pritunl Stable Repository
-baseurl=https://repo.pritunl.com/stable/yum/oraclelinux/8/
+baseurl=https://repo.pritunl.com/stable/yum/almalinux/9/
 gpgcheck=1
 enabled=1
 EOF
 
 gpg --keyserver hkp://keyserver.ubuntu.com --recv-keys 7568D9BB55FF9E5287D586017AE645C0CF8E292A
-gpg --armor --export 7568D9BB55FF9E5287D586017AE645C0CF8E292A > key.tmp; 
-sudo rpm --import key.tmp; rm -f key.tmp
+gpg --armor --export 7568D9BB55FF9E5287D586017AE645C0CF8E292A > key.tmp; sudo rpm --import key.tmp; rm -f key.tmp
 sudo yum -y install epel-release
 sudo yum -y update
-sudo yum -y install pritunl-client-electron
-sudo yum -y install jq
-sudo yum -y install wireguard-tools
+sudo yum -y install pritunl-client-electron wireguard-tools jq
+ 
 #Fix lỗi tự disconnect wireguard interface khi start profile pritunl
 sudo systemctl start systemd-resolved
 sudo systemctl enable systemd-resolved
@@ -39,16 +37,16 @@ first_id=$(echo "$id_field" | cut -d' ' -f1-3)
 pritunl-client list
 echo "ID of VPN profile is: $first_id"
 
-ba_ky_tu=${first_id:0:3}
+#ba_ky_tu=${first_id:0:3}
 
-pritunl-client enable $ba_ky_tu
+pritunl-client enable $first_id
 
 read -p "Nhấn Y và Enter để bật VPN profile: $first_id >" choice
 # Kiểm tra xem người dùng đã nhấn "Y" hay chưa
 if [ "$choice" == "Y" ] || [ "$choice" == "y" ]; then
   # Start profile VPN
-    pritunl-client start $ba_ky_tu -m wg
-    echo "ĐÃ KÍCH HOẠT VPN PROFILE ID: $first_id"
+    pritunl-client start $first_id -m wg
+    echo "ĐÃ KÍCH HOẠT VPN PROFILE ID: $first_id Wireguard tunnel.."
    
 else
     echo "Kết thúc chương trình."
